aa35afbe0c5e75cc5d6df6dac43bb568685c470e
Erik Hjelm Fjeldheim <erikhfj@stud.ntnu.no>
2024-10-17 18:02:30 +0200

    Test passed, pushing changes.

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,10 +1,8 @@
 ﻿class TicTacToe:
     def __init__(self):
-        self.board =  "I AM A BOARD"
-
-
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        #self.board =  "I AM A BOARD"
 
 
 if __name__ == '__main__':
     game = TicTacToe()
-    
